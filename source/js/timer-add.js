4e574b3a3a19633ab9096a743dbe8c632e33f112
Rodion <103366016+ZhRodion@users.noreply.github.com>
2023-04-01 16:23:07 +0400

    Исправляет ошибки

--- source/js/timer-add.js
+++ source/js/timer-add.js
@@ -1,9 +1,8 @@
 // Создание таймера по кнопке 
 const addButton = document.querySelector('.all-timer-container__button');
 const timerContainer = document.querySelector('.all-timer-container');
-const stopwatch = document.querySelector('.timer-container');
 
 addButton.addEventListener('click', function () {
-  const stopwatchCopy = stopwatch.cloneNode(true);
+  const stopwatchCopy = stopwatch.cloneNode();
   timerContainer.appendChild(stopwatchCopy);
 });
